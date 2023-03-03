cc26451334ea3f21288a6b098302e2c1293fcbb0
Manohar19A <manohar.chimata@arshaa.com>
2023-03-03 18:09:40 +0530

    Seconf commit

--- ParkingSystem.java
+++ ParkingSystem.java
@@ -1,17 +1,21 @@
 import java.util.Scanner;
-import java.io.*;
-import java.nio.file.Paths;
-import java.util.*;
 
 public class ParkingSystem {
-    private boolean[] parkingLot;
-    private int capacity;
+    private boolean[][] parkingLot;
+    private int numFloors;
+    private int numSpacesPerFloor;
     private int availableSpaces;
 
-    public ParkingSystem(int capacity) {
-        this.capacity = capacity;
-        this.availableSpaces = capacity;
-        this.parkingLot = new boolean[capacity];
+    public ParkingSystem(int numFloors, int numSpacesPerFloor) {
+        this.numFloors = numFloors;
+        this.numSpacesPerFloor = numSpacesPerFloor;
+        this.availableSpaces = numFloors * numSpacesPerFloor;
+        this.parkingLot = new boolean[numFloors][numSpacesPerFloor];
+        for (int i = 0; i < numFloors; i++) {
+            for (int j = 0; j < numSpacesPerFloor; j++) {
+                this.parkingLot[i] = new boolean[numSpacesPerFloor];
+            }
+        }
     }
 
     public boolean isParkingFull() {
@@ -22,81 +26,41 @@ public class ParkingSystem {
         if (isParkingFull()) {
             return false;
         }
-        for (int i = 0; i < capacity; i++) {
-            if (!parkingLot[i]) {
-                parkingLot[i] = true;
-                availableSpaces--;
-                System.out.println("Car parked at space " + (i + 1));
-                return true;
-            }
+        for (int i = 0; i < numFloors; i++) {
+            // for (int j = 0; j < numSpacesPerFloor; j++) {
+                for (int k = 0; k < numSpacesPerFloor; k++) {
+                    if (!parkingLot[i][k]) {
+                        parkingLot[i][k] = true;
+                        availableSpaces--;
+                        System.out.println("Car parked at floor " + (i+1) + " space " + (k+1));
+                        return true;
+                    }
+                }
+            // }
         }
         return false;
     }
 
-    public boolean leaveCar(int space) {
-        if (space < 1 || space > capacity || !parkingLot[space - 1]) {
+    public boolean leaveCar(int floor, int space) {
+        if (floor < 1 || floor > numFloors || space < 1 || space > numSpacesPerFloor || !parkingLot[floor-1][space-1]) {
             return false;
         }
-        parkingLot[space - 1] = false;
+        parkingLot[floor-1][space-1] = false;
         availableSpaces++;
-        System.out.println("Car left space " + space);
+        System.out.println("Car left floor " + floor + " space " + space);
         return true;
     }
-
     public int getAvailable() {
         return availableSpaces;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of floors: ");
+        int numFloors = scanner.nextInt();
         System.out.print("Enter parking lot capacity: ");
-        int capacity = scanner.nextInt();
-        ParkingSystem parkingSystem = new ParkingSystem(capacity);
-        HashMap<String, Integer> map = new HashMap<String, Integer>();
-        map.put("length", capacity);
-        map.put("available", parkingSystem.getAvailable());
-        File file = new File("./data.txt");
-        BufferedWriter bf = null;
-        try {
-            bf = new BufferedWriter(new FileWriter(file));
-            for (Map.Entry<String, Integer> entry : map.entrySet()) {
-                bf.write(entry.getKey() + "=" + entry.getValue());
-                bf.newLine();
-            }
-            bf.flush();
-
-        } catch (IOException e) {
-            System.out.println("Exception1");
-        } finally {
-            try {
-                bf.close();
-            } catch (IOException e) {
-                System.out.println("Exception2");
-            }
-        }
-        
-        // String extractedLine = Files.readAllLines(Paths.get("./data.txt")).get(4);
-        BufferedReader br = null;
-        try{
-            br = new BufferedReader(new FileReader(file));
-            String line = null;
-            
-            while((line= br.readLine()) != null){
-                System.out.println(line.length());
-                // String line32 = (String) FileUtils.readLines(file).get(31);
-                // String extractedLine = br.readAllLines(Paths.get("./data.txt")).get(1);
-                // String[] parts = line.split("=");
-
-                // // String line32 = (String) FileUtils .readLines(file).get(31);
-                // // System.out.println("line 81"+line.charAt(7));
-                // System.out.println("line 83 "+parts[1]);
-                // br.n
-            }
-            
-        }
-        catch(IOException e){
-            System.out.println("Exception3");
-        }
+        int numSpacesPerFloor = scanner.nextInt();
+        ParkingSystem parkingSystem = new ParkingSystem(numFloors, numSpacesPerFloor);
         while (true) {
             System.out.print("Enter command (park/leave/count/exit): ");
             String command = scanner.next();
@@ -105,20 +69,29 @@ public class ParkingSystem {
                     System.out.println("Sorry, parking lot is full.");
                 }
             } else if (command.equals("leave")) {
+                System.out.print("Enter floor number: ");
+                int floor = scanner.nextInt();
+                // System.out.print("Enter row number: ");
+                // int row = scanner.nextInt();
                 System.out.print("Enter space number: ");
                 int space = scanner.nextInt();
-                if (!parkingSystem.leaveCar(space)) {
-                    System.out.println("Invalid space number or space is already empty.");
+                if (!parkingSystem.leaveCar(floor, space)) {
+                    System.out.println("Invalid floor/row/space number or space is already empty.");
                 }
-            } else if (command.equals("count")) {
-                System.out.print("Park slots left: " + parkingSystem.getAvailable() + "\n");
+            }
+            else if (command.equals("count")) {
+                System.out.print("Parking slots left: " + parkingSystem.getAvailable() + "\n");
 
             } else if (command.equals("exit")) {
+                System.out.println("ThankYou!");
                 break;
             } else {
-                System.out.println("Invalid command.");
+                System.out.println("Invalid Command");
+                
             }
         }
         scanner.close();
     }
 }
+
+        
